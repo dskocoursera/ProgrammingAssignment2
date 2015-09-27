74f1739f957ff4b93d351f8c50b587947bb1ec44
dskocoursera <dsko.coursera@gmail.com>
2015-09-27 21:35:12 +1300

    complete

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Create a matrix x for which the inverse calculated via solve(x) can be cached.
+## Parameters:
+## x - a matrix, defaults to a 1x1 NA matrix.
+##
+## Return: 
+## A list of the following functions for manipulating the matrix
+## set - set a new value as the matrix
+## get - get the current value of the matrix
+## getSolve - get the cached inverse, or NULL if there is no inverse cached
+## setSolve - set the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cachedSolve <- NULL
+    set <- function(y) {
+        x <<- y
+        cachedSolve <<- NULL ## since we've set a new matrix, clear the cached inverse
+    }
+    get <- function() x
+    setSolve <- function(solveResult) cachedSolve <<- solveResult
+    getSolve <- function() cachedSolve
+    list(set = set, get = get,
+         setSolve = setSolve,
+         getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+## Return the inverse of the matrix x. If the inverse is cached, return the cached value,
+## else calculate the inverse, cache it, and return it.
+##
+## Parameters:
+## x - the list of functions returned from makeCacheMatrix, when the matrix was made.
+## ... - extra parameters for solve()
+##
+## Return: 
+## A matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    cachedSolve <- x$getSolve()
+    if (!is.null(cachedSolve)) {
+        message("getting cached data")
+        return(cachedSolve)
+    }
+    matrix <- x$get()
+    result <- solve(matrix)
+    x$setSolve(result)
+    result
 }
